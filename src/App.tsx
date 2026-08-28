7646537d0edd5072596909ca6e6bc7820878826f
JonesPrado <jockley340@gmail.com>
2026-08-28 08:26:21 -0300

    feat: iniciando repositório com estrutura vite em typescript, react e tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <main className="bg-amber-200 min-h-screen flex flex-col justify-center items-center">
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </main>
+  )
+}
+
+export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+function Button() {
+    return(
+        <button className="p-6 rounded-3xl bg-amber-500 hover:bg-amber-700 m-3 transition-colors">
+            Botãozinho
+        </button>
+    )
+}
+
+export default Button
